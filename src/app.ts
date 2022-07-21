499bb7e68cb92e8f8b67dec5b87bb95100c3c10e
João Pedro Patrocinio <patrocinio.jpedro@gmail.com>
2022-07-21 15:06:17 -0300

    Testing Github Integration

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,6 @@ import { exit } from "process";
 console.log("Hello, world!");
 console.log("Testing Github Integration #1");
 console.log("Testing Github Integration #2");
+console.log("Testing Github Integration #3");
 
 exit(0);
